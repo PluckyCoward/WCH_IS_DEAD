08e2138475ae36f3098e027072545ef9ebe9da7e
Howard WU <Wchoward@users.noreply.github.com>
2020-01-27 14:31:00 +0800

    DP

---
+++ W005. Longest Palindromic Substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int dp[1000][1000]={0};
+        int len = s.size();
+        if (len == 0 || len == 1)
+            return s;
+        int start = 0;
+        int max = 1;
+        for(int i = 0; i < len; i++){
+            dp[i][i] = 1;
+            if (i < len - 1 && s[i] == s[i+1]){
+                dp[i][i+1] = 1;
+                max = 2;
+                start = i;
+            }
+        }
+        for (int l = 3; l <= len; l++) {
+            for (int i = 0; i + l - 1 < len; i++) {
+                int j = i + l - 1;
+                if (s[i] == s[j] && dp[i+1][j-1] == 1) {
+                    dp[i][j] = 1;
+                    start = i;
+                    max = l;
+                }
+            }
+        }
+        return s.substr(start, max);
+    }
+};
