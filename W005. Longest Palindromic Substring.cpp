6ce2d4f60eac535cc0231391c800db11a64284af
Howard WU <Wchoward@users.noreply.github.com>
2020-01-28 11:36:15 +0800

    DP;Manacher

--- W005. Longest Palindromic Substring.cpp
+++ W005. Longest Palindromic Substring.cpp
@@ -1,3 +1,4 @@
+//DP
 class Solution {
 public:
     string longestPalindrome(string s) {
@@ -26,5 +27,53 @@ public:
             }
         }
         return s.substr(start, max);
+    }
+};
+
+//Manacher
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int len = (int) s.length();
+        if (len < 1) {
+            return "";
+        }
+        
+        //new s with "#"
+        string s1;
+        for (int i = 0; i < len; i++) {
+            s1 += "#";
+            s1 += s[i];
+        }
+        s1 += "#";
+        
+        len = (int) s1.length();
+        int maxRight = 0; //最右回文右边界位置
+        int pos = 0; //对称中心位置
+        int maxRL = 0; //最大回文半径长度
+        int maxPos = 0; //最大回文串对称轴位置
+        int* RL = new int[len]; //回文半径数组
+        memset(RL, 0, len * sizeof(int));
+        for (int i = 0; i < len; i++) {
+            if (i < maxRight) {
+                RL[i] = min(RL[2 * pos - i], maxRight - i);
+            }
+            else{
+                RL[i] = 1;
+            }
+            while (i - RL[i] >= 0 && i + RL[i] < len && s1[i - RL[i]] == s1[i + RL[i]]) {
+                RL[i] += 1;
+            }
+            
+            if (RL[i] + i - 1 > maxRight) {
+                maxRight = RL[i] + i - 1;
+                pos = i;
+            }
+            if (maxRL <= RL[i]) {
+                maxRL = RL[i];
+                maxPos = i;
+            }
+        }
+        return s.substr((maxPos - maxRL + 1) / 2, maxRL - 1);
     }
 };
