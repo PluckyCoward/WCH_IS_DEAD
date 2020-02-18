d5a01bfe092ad09be4715f46c28a27bc430331e0
Howard WU <Wchoward@users.noreply.github.com>
2020-02-18 11:55:19 +0800

    Array; Backtracking

---
+++ W040. Combination Sum II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    vector<vector<int>> res;
+    vector<int> path;
+    vector<int> candidates;
+public:
+    void DFS(int start, int target){
+        if (target == 0) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = start; i < candidates.size() && target - candidates[i] >= 0; i++) {
+            if (i > start && candidates[i] == candidates[i - 1])
+                continue;
+            path.push_back(candidates[i]);
+            DFS(i + 1, target - candidates[i]);
+            path.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target){
+        sort(candidates.begin(), candidates.end());
+        this -> candidates = candidates;
+        DFS(0, target);
+        return res;
+    }
+};
