4ba3a5fdac6c6bfc8a7a2dce498ae44c50017c65
Howard WU <Wchoward@users.noreply.github.com>
2020-03-31 13:47:46 +0800

    Binary Search

---
+++ W744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        sort(letters.begin(), letters.end());
+        int idx = upper_bound(letters.begin(), letters.end(), target) - letters.begin();
+        if (idx == letters.size())
+            return letters[0];
+        return letters[idx];
+    }
+};
+
