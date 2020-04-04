54055d6f3d14455c487120b95730b993fbada5a0
Howard WU <Wchoward@users.noreply.github.com>
2020-04-04 17:18:39 +0800

    Tree

---
+++ W116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL)
+            return NULL;
+        if (root->left)
+            root->left->next = root->right;
+        if (root->right)
+            root->right->next = (root->next ? root->next->left : NULL);
+        connect(root->left);
+        connect(root->right);
+        return root;        
+    }
+};
