d6f81b5df82cb8b7ddbfa34c435ab0c74b5e451e
plucky <1828151761@qq.com>
2020-04-06 20:24:28 +0800

    二分;递归

---
+++ T050. Pow(x, n).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double pow(double x, int n){
+        if(n == 0)
+            return 1.0;
+        int half = pow(x,n / 2);
+        if(n % 2)
+            return x * half * half;
+        return half * half;
+    }
+    double myPow(double x, int n) {
+        long long ln = n;
+        if(ln < 0){
+            x = 1 / x;
+            ln = -ln;
+        }
+        return pow(x,ln);
+    }
+};
