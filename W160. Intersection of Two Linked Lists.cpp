de072c54ce4af16ecbaa3531bb0a3967bf61f454
Howard WU <Wchoward@users.noreply.github.com>
2020-04-29 12:52:03 +0800

    Two Pointers; Linked List

---
+++ W160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,19 @@
+//Two pointers
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
+        while (p1 != p2) {
+            if (p1 == NULL)
+                p1 = headB;
+            else
+                p1 = p1->next;
+            if (p2 == NULL)
+                p2 = headA;
+            else
+                p2 = p2->next;            
+        }
+        return p1;
+    }
+};
