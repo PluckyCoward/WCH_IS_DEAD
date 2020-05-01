20d8390ca9257349692cf00b59bccfaa4a4f3596
Howard WU <Wchoward@users.noreply.github.com>
2020-05-01 17:59:47 +0800

    DFS; BFS; Tree

--- W102. Binary Tree Level Order Traversal.cpp
+++ W102. Binary Tree Level Order Traversal.cpp
@@ -22,26 +22,25 @@ public:
 //bfs
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root){
+    vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        vector<int> level;
         if (root == NULL)
             return res;
         queue<TreeNode*> q;
         q.push(root);
         while (!q.empty()) {
+            vector<int> level;
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 TreeNode* curr = q.front();
-                level.push_back(curr->val);
                 q.pop();
+                level.push_back(curr->val);
                 if (curr->left)
                     q.push(curr->left);
                 if (curr->right)
                     q.push(curr->right);
             }
             res.push_back(level);
-            level.clear();
         }
         return res;
     }
