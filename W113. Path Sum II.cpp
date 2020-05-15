f6c960b87b1ffec2730162c11d067554c943a0c4
Howard WU <Wchoward@users.noreply.github.com>
2020-05-15 12:44:37 +0800

    Tree; DFS;

--- W113. Path Sum II.cpp
+++ W113. Path Sum II.cpp
@@ -2,24 +2,22 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    
+
+    void dfs(TreeNode* node, int sum, vector<int> tmp) {
+        tmp.push_back(node->val);
+        if (node->left == NULL && node->right == NULL && sum - node->val == 0)
+            res.push_back(tmp);
+        if (node->left)
+            dfs(node->left, sum - node->val, tmp);
+        if (node->right)
+            dfs(node->right, sum - node->val, tmp);
+    }
+
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        if (!root)
+        if (root == NULL)
             return res;
         vector<int> tmp;
         dfs(root, sum, tmp);
         return res;
-        
-    }
-    
-    void dfs(TreeNode* root, int sum, vector<int> tmp){
-        tmp.push_back(root->val);
-        if (root->left == NULL && root->right ==NULL && sum - root->val == 0) {
-            res.push_back(tmp);
-        }
-        if (root->left)
-            dfs(root->left, sum - root->val, tmp);
-        if (root->right)
-            dfs(root->right, sum - root->val, tmp);
     }
 };
