7561e226f4828b853e32d48b446180ec213edc42
Howard WU <Wchoward@users.noreply.github.com>
2020-05-24 10:22:10 +0800

    DFS; Tree; Array

--- W106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ W106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    vector<int> in,post;
+    vector<int> in, post;
     
     int index;
     
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = inorder.size();
-        if (!n) return NULL;sy
         in = inorder;
         post = postorder;
         index = n - 1;
-        TreeNode* res = build(0, n - 1);
+        TreeNode* res = dfs(0, index);
         return res;
     }
     
-    TreeNode* build(int start, int end){
-        if (start > end)
+    TreeNode* dfs(int begin, int end) {
+        if (begin > end)
             return NULL;
         int root = post[index--];
-        TreeNode* t = new TreeNode(root);
-        int pos = find(in.begin() + start, in.end() + end, root) - in.begin();
-        t->right = build(pos + 1, end);
-        t->left = build(start, pos - 1);
-        return t;
+        TreeNode* node = new TreeNode(root);
+        int pos = find(in.begin(), in.end(), root) - in.begin();
+        node->right = dfs(pos + 1, end);
+        node->left = dfs(begin, pos - 1);
+        return node;
     }
 };
