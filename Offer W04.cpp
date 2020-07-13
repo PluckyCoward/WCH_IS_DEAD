16ade20ac714b9447bdb060df510ea2df47a4c5c
Howard WU <Wchoward@users.noreply.github.com>
2020-07-13 22:11:35 +0800

    Array; Two Pointers

---
+++ Offer W04.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        int m = matrix.size(), n = matrix[0].size();
+        int row = m - 1;
+        int col = 0;
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] < target) 
+                col++;
+            else if (matrix[row][col] > target)
+                row--;
+            else
+                return true;
+        }
+        return false;
+    }
+};
