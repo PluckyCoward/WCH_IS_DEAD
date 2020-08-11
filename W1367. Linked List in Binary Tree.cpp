e9e04c3ccab2dc4047238740619014bf6e72492f
Howard WU <Wchoward@users.noreply.github.com>
2020-08-11 17:07:12 +0800

    DFS; Tree; Linked List

---
+++ W1367. Linked List in Binary Tree.cpp
@@ -0,0 +1,20 @@
+//dfs
+class Solution {
+public:
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if (head == NULL)
+            return true;
+        if (root == NULL)
+            return false;
+        return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+    bool dfs (ListNode* head, TreeNode* root) {
+        if (head == NULL)
+            return true;
+        if (root == NULL)
+            return false;
+        if (head->val != root->val)
+            return false;
+        return dfs(head->next, root->left) || dfs(head->next, root->right);
+    }
+};
