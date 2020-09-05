fe0e8740704f2761efc758e7eb5ee30341a44a96
plucky <1828151761@qq.com>
2020-09-05 20:18:08 +0800

    DP

--- T1025. Divisor Game.cpp
+++ T1025. Divisor Game.cpp
@@ -1,7 +1,26 @@
+//dp
 class Solution {
 public:
     bool divisorGame(int N) {
-        if(N%2==0)return true;
-        return false;
+        int dp[1001]= {};
+        dp[1] = 0;
+        dp[2] = 1;
+        for(int i = 3;i <= N; i++){
+            for(int j = 1;j < i;j++){
+                if(N % j == 0 && dp[j]){
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[N];
+    }
+};
+
+//math
+class Solution {
+public:
+    bool divisorGame(int N) {
+        return N % 2 == 0;
     }
 };
