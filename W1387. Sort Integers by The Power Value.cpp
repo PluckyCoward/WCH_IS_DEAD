9b578681da9450730614577a69ca1ff048ebde5f
Howard WU <Wchoward@users.noreply.github.com>
2020-09-25 17:51:32 +0800

    Sort; Recursive; Memory

--- W1387. Sort Integers by The Power Value.cpp
+++ W1387. Sort Integers by The Power Value.cpp
@@ -22,3 +22,31 @@ public:
         return res[k - 1];
     }
 };
+
+//记忆化
+unordered_map<int, int> f;
+
+int getK(int x) {
+    if (f.find(x) != f.end()) return f[x];
+    if (x == 1) return f[x] = 0;
+    if (x % 2 == 1)
+        return f[x] = getK(3 * x + 1) + 1;
+    else return f[x] = getK(x / 2) + 1;
+}
+
+bool cmp(int a, int b) {
+    if (getK(a) != getK(b)) return getK(a) < getK(b);
+    else
+        return a < b;
+}
+class Solution {
+public:
+    int getKth(int lo, int hi, int k) {
+        vector<int> res;
+        for (int i = lo; i <= hi; i++) {
+            res.push_back(i);
+        }
+        sort(res.begin(), res.end(), cmp);
+        return res[k - 1];
+    }
+};
