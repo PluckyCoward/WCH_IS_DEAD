9636da600b8033660bd3c2aecbfe8b622f54d8ff
Howard WU <Wchoward@users.noreply.github.com>
2020-10-15 15:28:08 +0800

    Tree; DFS

--- W116. Populating Next Right Pointers in Each Node.cpp
+++ W116. Populating Next Right Pointers in Each Node.cpp
@@ -17,3 +17,46 @@ public:
         return node;
     }
 };
+
+//dfs
+class Solution {
+public:
+    Node* connect(Node* root) {
+        dfs(root);
+        return root;
+    }
+
+    void dfs(Node* node) {
+        if (!node) return;
+        if (node->left)
+            node->left->next = node->right;
+        if (node->right)
+            node->right->next = node->next ? node->next->left : NULL;
+        dfs(node->left);
+        dfs(node->right);
+    }
+};
+
+//Queue
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int cnt = q.size();
+            for(int i = 0; i < cnt; i++) {
+                Node* node = q.front();
+                q.pop();
+                if(i != cnt - 1)
+                    node->next = q.front();
+                if (node->left != NULL)
+                    q.push(node->left);
+                if (node->right != NULL)
+                    q.push(node->right);
+            }
+        }
+        return root;
+    }
+};
