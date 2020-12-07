d59b1699e078dea185802b24c506838c9657fe85
Howard WU <Wchoward@users.noreply.github.com>
2020-12-07 11:19:02 +0800

    Greedy; Bitwise operators

--- W861. Score After Flipping Matrix.cpp
+++ W861. Score After Flipping Matrix.cpp
@@ -3,13 +3,15 @@ public:
     int matrixScore(vector<vector<int>>& A) {
         int row = A.size();
         int col = A[0].size();
+        //每行的第一个都要为1
         for (int i = 0; i < row; i++) {
             if (A[i][0] == 0) {
                 for (int j = 0; j < col; j++) {
-                    A[i][j] ^= 1; 
+                    A[i][j] ^= 1;
                 }
             }
         }
+        //从第二列开始每列的1的个数>0
         int sum = row;
         for (int j = 1; j < col; j++) {
             int tmp = 0;
