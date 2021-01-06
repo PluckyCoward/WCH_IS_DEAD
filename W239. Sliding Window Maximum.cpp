f860de71b9094ea49ee8e8f28b7b86f17cc946bd
Howard WU <Wchoward@users.noreply.github.com>
2021-01-06 23:34:48 +0800

    Heap; Sliding Window

---
+++ W239. Sliding Window Maximum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<pair<int, int>> q;
+        for (int i = 0; i < k; ++i) {
+            q.emplace(nums[i], i);
+        }
+        vector<int> res = {q.top().first};
+        for (int i = k; i < n; ++i) {
+            q.emplace(nums[i], i);
+            while (q.top().second <= i - k) {
+                q.pop();
+            }
+            res.push_back(q.top().first);
+        }
+        return res;
+    }
+};
