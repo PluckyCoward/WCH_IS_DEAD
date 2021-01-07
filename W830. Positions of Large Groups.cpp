c2e221d15e83d540e6bcae45899614bad50f4552
Howard WU <Wchoward@users.noreply.github.com>
2021-01-07 16:35:30 +0800

    Array; Easy

---
+++ W830. Positions of Large Groups.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        vector<vector<int>> res;
+        int n = s.size();
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 || s[i] != s[i + 1]) {
+                if (num > 2)
+                    res.push_back({i - num + 1, i});
+                num = 1;
+            }
+            else
+                num++;
+        }
+        return res;
+    }
+};
